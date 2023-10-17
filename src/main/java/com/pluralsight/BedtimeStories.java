2972f60fa1046b473010ebe210961c9d67b5f7db
Andi <alexandra.e.burke@gmail.com>
2023-10-17 09:48:24 -0400

    Finished Project

--- src/main/java/com/pluralsight/BedtimeStories.java
+++ src/main/java/com/pluralsight/BedtimeStories.java
@@ -7,24 +7,85 @@ public class BedtimeStories {
     //tell the scanner where to find the file
     //print file contents
     public static void main(String[] args) {
-        try {
+            System.out.println("Choose Your Story: \n" + "1: Goldilocks \n" +
+                    "2: Hansel and Gretel \n" +
+                    "3: Mary Had a Little Lamb");
+            Scanner storyChoice = new Scanner(System.in);
+            int userChoice = storyChoice.nextInt();
+            switch (userChoice) {
+                case 1: {
+                    readGoldilocks();
+                    break;
+                }
+                case 2: {
+                    readHanselAndGretel();
+                break;
+                }
+                case 3: {
+                    readMaryHadALittleLamb();
+                    break;
+                }
+            }
 // create a FileInputStream object pointing to
 // a specific file
-            FileInputStream fis = new FileInputStream("src/main/resources/hansel_and_gretel.txt");
+
+    }
+    public static void readGoldilocks() {
+        int countLine = 0;
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/goldilocks.txt");
 // create a Scanner to reference the file to be read
             Scanner scanner = new Scanner(fis);
             String story;
 // read until there is no more data
             while (scanner.hasNextLine()) {
+                countLine++;
                 story = scanner.nextLine();
-                System.out.println(story);
+                System.out.println(countLine + " " + story);
             }
 // close the scanner and release the resources
             scanner.close();
-        }
-        catch (IOException e) {
-// display stack trace if there was an error
-            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
-}
+        public static void readHanselAndGretel() {
+        int countLine = 0;
+            try {
+                FileInputStream fis = new FileInputStream("src/main/resources/hansel_and_gretel.txt");
+// create a Scanner to reference the file to be read
+                Scanner scanner = new Scanner(fis);
+                String story;
+// read until there is no more data
+                while (scanner.hasNextLine()) {
+                    countLine++;
+                    story = scanner.nextLine();
+                    System.out.println(countLine + " " + story);
+                }
+// close the scanner and release the resources
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+            public static void readMaryHadALittleLamb() {
+        int countLine = 0;
+                try {
+                    FileInputStream fis = new FileInputStream("src/main/resources/mary_had_a_little_lamb.txt");
+// create a Scanner to reference the file to be read
+                    Scanner scanner = new Scanner(fis);
+                    String story;
+// read until there is no more data
+                    while (scanner.hasNextLine()) {
+                        countLine++;
+                        story = scanner.nextLine();
+                        System.out.println(countLine + " " + story);
+                    }
+// close the scanner and release the resources
+                    scanner.close();
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
